d9d588ada178856b46b3352daaad6a893ed52d0a
Rhman101 <rhuysen@gmail.com>
2019-03-25 05:32:56 +0200

    add Redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import configureStore from './store/configureStore';
 import Dashboard from './Components/Dashboard';
 import GamePage from './Components/GamePage';
 import GameSetUpPage from './Components/GameSetUpPage';
 import LandingPage from './Components/LandingPage';
 import NotFoundPage from './Components/NotFoundPage';
+
+// I assume we won't need these for production, so I will put them here for removal later.
+import {
+  additionComplete,
+  subtractionComplete,
+  multiplicationComplete,
+  divisionComplete,
+} from './actions/achievementsActions';
+import { characterChoice } from './actions/gameStateActions';
 
 class App extends Component {
   render() {
@@ -27,5 +37,18 @@ class App extends Component {
     );
   }
 }
+
+const store = configureStore();
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(additionComplete(true));
+store.dispatch(additionComplete(false));
+store.dispatch(subtractionComplete(true));
+store.dispatch(multiplicationComplete(true));
+store.dispatch(divisionComplete(true));
+store.dispatch(characterChoice(1));
 
 export default App;
