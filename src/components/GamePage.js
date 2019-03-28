986a7e68d60fab364fd2aedee6264d2d74763e9a
mawarijo <mdopure@gmail.com>
2019-03-27 21:57:55 -0700

    use character and area data

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const GamePage = ({ location: { state } }) => (
-  <div>
-    <h2>This is the game page</h2>
-    <p>My character is {state.character}</p>
-    <p>This is area {state.area}</p>
-  </div>
-);
+import AREAS from '../constants/areas';
+import CHARACTERS from '../constants/characters';
+
+const GamePage = ({ location: { state } }) => {
+  const character = CHARACTERS.find(char => char.id === state.character);
+  const area = AREAS.find(ar => ar.id === state.area);
+  return (
+    <div>
+      <h2>{area.name}</h2>
+      <p>You are {character.name}.</p>
+    </div>
+  );
+};
 
 export default GamePage;
