c4c85bb92cc17986a465cebfe867e8a027787494
Rhman101 <rhuysen@gmail.com>
2019-04-08 16:23:42 +0200

    I wrapped the app in the Provider component and edited the gameSetupPage component such that it is connected to the Redux Store and can dispatch actions. I did this as an example of sorts. See comments for details.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import configureStore from './store/configureStore';
 import Dashboard from './components/Dashboard';
 import GamePage from './components/GamePage';
 import GameSetupPage from './components/GameSetupPage';
 import LandingPage from './components/LandingPage';
 import NotFoundPage from './components/NotFoundPage';
-
-// I assume we won't need these for production, so I will put them here for removal later.
-import {
-  additionComplete,
-  subtractionComplete,
-  multiplicationComplete,
-  divisionComplete,
-} from './actions/achievementsActions';
-import { characterChoice } from './actions/gameStateActions';
 
 class App extends Component {
   render() {
@@ -36,18 +26,5 @@ class App extends Component {
     );
   }
 }
-
-const store = configureStore();
-
-store.subscribe(() => {
-  // console.log(store.getState());
-});
-
-store.dispatch(additionComplete(true));
-store.dispatch(additionComplete(false));
-store.dispatch(subtractionComplete(true));
-store.dispatch(multiplicationComplete(true));
-store.dispatch(divisionComplete(true));
-store.dispatch(characterChoice(1));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// I assume we won't need these for production, so I will put them here for removal later.
+import {
+  additionComplete,
+  subtractionComplete,
+  multiplicationComplete,
+  divisionComplete,
+} from './actions/achievementsActions';
+import { characterChoice } from './actions/gameStateActions';
+
+const store = configureStore();
+
+store.subscribe(() => {
+  // console.log(store.getState());
+});
+
+store.dispatch(additionComplete(true));
+store.dispatch(additionComplete(false));
+store.dispatch(subtractionComplete(true));
+store.dispatch(multiplicationComplete(true));
+store.dispatch(divisionComplete(true));
+store.dispatch(characterChoice(1));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
