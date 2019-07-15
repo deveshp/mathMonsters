f2d8abbf84a42c366f8986cde07c022918893ef8
Rhman101 <rhuysen@gmail.com>
2019-07-15 06:23:50 +0200

    Ruan: I made many updates. The characters now move, the win/lose conditions calculate properly, etc. There is still quite a bit of work to be done in terms of styling, etc. And I want to have each area now go through the easy to medium to hard process.

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -16,11 +16,34 @@ class GamePage extends Component {
       math: {
         ...operation(this.props.location.state.area),
         userInput: ''
-      }
+      },
+      attempted: false,
+      correct: 0,
+      incorrect: 0,
+      toggle: false,
+      CSStimeState: 'before', // 'during', 'reset'
+      started: false,
+      initialWidthForCharsMovement: document.documentElement.clientWidth
     }
     this.textInputChange = this.textInputChange.bind(this);
     this.submitAnswer = this.submitAnswer.bind(this);
+    this.timeoutBarCSS = this.timeoutBarCSS.bind(this);
+    this.areaComplete = this.areaComplete.bind(this);
+    this.monsterMovementCSS = this.monsterMovementCSS.bind(this);
   }
+
+  areaComplete = (area) => {
+    if (area === 1 ) {
+      this.props.dispatch(updateAreaComplete('addition', true));
+    } else if (area === 2) {
+      this.props.dispatch(updateAreaComplete('subtraction', true));
+    } else if (area === 3) {
+      this.props.dispatch(updateAreaComplete('multiplication', true));
+    } else if (area === 4) {
+      this.props.dispatch(updateAreaComplete('division', true));
+    }
+    this.props.history.push('/game_setup');
+  };
 
   textInputChange(event) {
     event.persist();
@@ -33,63 +56,98 @@ class GamePage extends Component {
     }))
   }
 
-  submitAnswer(event) {
+  monsterMovementCSS() {
+    return {
+      style: {marginLeft: this.state.incorrect / this.state.math.hitPoints * (this.state.initialWidthForCharsMovement - 150)},
+      className: 'monsterPicDiv'
+    }
+  }
+
+  characterMovementCSS() {
+    return {
+      style: {marginLeft: this.state.correct / this.state.math.numberOfQuestions * (this.state.initialWidthForCharsMovement - 150)},
+      className: 'characterPicDiv'
+    }
+  }
+
+  timeoutBarCSS() {
+    if (this.state.CSStimeState === 'before') {
+      return {
+        style: {height: 120},
+        className: `timeOutBar`
+      }
+    } else if (this.state.CSStimeState === 'during') {
+      return {
+        style: {animationName: 'boxHeight', animationDuration: `${this.state.math.seconds}s`},
+        className: `timeOutBar `
+      }
+    } else if (this.state.CSStimeState === 'reset') {
+      return {
+        className: `timeOutBar`,
+        style: {animationName: 'boxHeightTwo', animationDuration: `${this.state.math.seconds}s`}
+      }
+    }
+  }
+
+  async submitAnswer (event) {
     event.preventDefault();
     if (Number(this.state.math.userInput) === this.state.math.correctResult) {
-      this.setState(() => ({
+      await this.setState(() => ({
+        CSStimeState: this.state.started === false ? 'during' : 'reset',
         math: {
           ...operation(this.props.location.state.area),
-          userInput: ''
-        }
+          userInput: '',
+        },
+        started: this.state.started === false && true,
+        correct: this.state.attempted === false ? this.state.correct + 1 : this.state.correct,
+        attempted: false
       }))
-      // document.getElementById("answerInput").addEventListener("keyup", function(event) {
-      //   event.preventDefault();
-      //   if (event.keyCode == 13) {
-      //       console.log('something');
-      //   }})
     } else {
-      this.setState(() => ({
+      await this.setState(() => ({
+        attempted: true,
+        incorrect: this.state.attempted === false ? this.state.incorrect + 1 : this.state.incorrect,
+        // started: this.state.started === false && true,
+        // CSStimeState: this.state.started === false ? 'during' : 'reset',
         math: {
           ...this.state.math,
-          userInput: ''
+          userInput: '',
         }
       }))
     }
-  }
-  render() {
-    const areaComplete = (area) => {
-      if (area === 1 ) {
-        this.props.dispatch(updateAreaComplete('addition', true));
-      } else if (area === 2) {
-        this.props.dispatch(updateAreaComplete('subtraction', true));
-      } else if (area === 3) {
-        this.props.dispatch(updateAreaComplete('multiplication', true));
-      } else if (area === 4) {
-        this.props.dispatch(updateAreaComplete('division', true));
-      }
-      this.props.history.push('/game_setup');
-    };
 
+    // Win and lose conditions
+    if (this.state.incorrect === this.state.math.hitPoints) {
+      this.props.history.push('/game_setup');
+    } else if (this.state.correct === this.state.math.numberOfQuestions) {
+      this.areaComplete(this.state.area.id);
+    }
+  }
+
+  render() {
+    // console.log(this.state);
     return (
       <div>
         <h2>{this.state.area.name}</h2>
         <h4>{this.state.area.operation}</h4>
         {/*<p>You are {character.name}.</p>*/}
-        <div id='monsterPicDiv'></div>
-        <p></p>
-        <div id='characterPicDiv'></div>
+        <div {...this.monsterMovementCSS()}></div>
+        <div {...this.characterMovementCSS()}></div>
         <p></p>
         <div id='mathBox'>
         <p id='questionText'>{this.state.math.questionText}</p>
+        <p>{this.state.math.seconds} seconds </p>
+        <p>correct: {this.state.correct} / {this.state.math.numberOfQuestions}</p>
+        <p>incorrect: {this.state.incorrect} / {this.state.math.hitPoints}</p>
         <form onSubmit={this.submitAnswer}>
           <input type='text' value={this.state.math.userInput} onChange={this.textInputChange} />
           <button type='submit' value='submit'>submit</button>
         </form>
           <div id="timeOutBorder">
-          <div id="timeOutBar"></div>
+          <div {...this.timeoutBarCSS()} onClick={() => this.setState({toggle: !this.state.toggle})}></div>
           </div>
+          {<button onClick={() => {this.areaComplete(this.state.area.id)}}>Complete Area</button>}
         </div>
-        {<button onClick={() => {areaComplete(this.state.area.id)}}>Complete Area</button>}
+        
       </div>
     );
   };
--- src/constants/operations.js
+++ src/constants/operations.js
@@ -1,47 +1,115 @@
-const additionFunction = () => {
+const additionFunction = (difficulty = 'easy') => {
     let varOne = (Math.floor((Math.random() * 12) + 1));
     let varTwo = (Math.floor((Math.random() * 12) + 1));
+    let seconds, numberOfQuestions, hitPoints;
+    if (difficulty === 'easy') {
+        seconds = 10;
+        numberOfQuestions = 60;
+        hitPoints = 30;
+    } else if (difficulty === 'medium') {
+        seconds = 9;
+        numberOfQuestions = 80;
+        hitPoints = 30;
+    } else {
+        seconds = 8;
+        numberOfQuestions = 100;
+        hitPoints = 20; 
+    }
     return {
         correctResult: varOne + varTwo,
-        questionText: `What is ${varOne} + ${varTwo}?`
+        questionText: `What is ${varOne} + ${varTwo}?`,
+        seconds,
+        numberOfQuestions,
+        hitPoints
     }
 }
 
-const subtractionFunction = () => {
+const subtractionFunction = (difficulty = 'easy') => {
     let varOne = (Math.floor((Math.random() * 24) + 1));
     let varTwo = (Math.floor((Math.random() * 12) + 1) + varOne);
+    let seconds, numberOfQuestions, hitPoints;
+    if (difficulty === 'easy') {
+        seconds = 11;
+        numberOfQuestions = 50;
+        hitPoints = 25;
+    } else if (difficulty === 'medium') {
+        seconds = 10;
+        numberOfQuestions = 65;
+        hitPoints = 25;
+    } else {
+        seconds = 9; 
+        numberOfQuestions = 100;
+        hitPoints = 20;
+    }
     return {
         correctResult: varTwo - varOne,
-        questionText: `What is ${varTwo} - ${varOne}?`
+        questionText: `What is ${varTwo} - ${varOne}?`,
+        seconds,
+        numberOfQuestions,
+        hitPoints
     }
 }
 
-const multiplicationFunction = () => {
+const multiplicationFunction = (difficulty = 'seconds') => {
     let varOne = (Math.floor((Math.random() * 12) + 1));
     let varTwo = (Math.floor((Math.random() * 12) + 1));
+    let seconds, numberOfQuestions, hitPoints;
+    if (difficulty === 'easy') {
+        seconds = 10;
+        numberOfQuestions = 60;
+        hitPoints = 30;
+    } else if (difficulty === 'medium') {
+        seconds = 9;
+        numberOfQuestions = 80;
+        hitPoints = 30;
+    } else {
+        seconds = 8; 
+        numberOfQuestions = 100;
+        hitPoints = 20;
+    }
     return {
         correctResult: varTwo * varOne,
-        questionText: `What is ${varTwo} * ${varOne}?`
+        questionText: `What is ${varTwo} * ${varOne}?`,
+        seconds,
+        numberOfQuestions,
+        hitPoints
     }
 }
 
-const divisionFunction = () => {
+const divisionFunction = (difficulty = 'easy') => {
     let varOne = (Math.floor((Math.random() * 12) + 1));
-    let varTwo = (Math.floor((Math.random() * 12) + 1));
+    let varTwo = (Math.floor((Math.random() * 12) + 1));    
+    let seconds, numberOfQuestions, hitPoints;
+    if (difficulty === 'easy') {
+        seconds = 12;
+        numberOfQuestions = 50;
+        hitPoints = 20;
+    } else if (difficulty === 'medium') {
+        seconds = 11;
+        numberOfQuestions = 60;
+        hitPoints = 25
+    } else {
+        seconds = 10;
+        numberOfQuestions = 80;
+        hitPoints = 16;
+    }
     return {
         correctResult: varOne, 
-        questionText: `What is ${varOne * varTwo} ÷ ${varTwo}?`
+        questionText: `What is ${varOne * varTwo} ÷ ${varTwo}?`,
+        seconds, 
+        numberOfQuestions,
+        hitPoints
     }
 }
 
-export const operation = (input) => {
+export const operation = (input, difficulty) => {
     if (input === 1) {
-        return additionFunction();
+        return additionFunction(difficulty);
     } if (input === 2) {
-        return subtractionFunction();
+        return subtractionFunction(difficulty);
     } if (input === 3) {
-        return multiplicationFunction();
+        return multiplicationFunction(difficulty);
     } else {
-        return divisionFunction();
+        return divisionFunction(difficulty);
     }
 }
